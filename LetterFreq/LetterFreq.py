9b5d1e47cd14e51e7e82e0cd7edefb3cbb35e086
khoicrtp <60875449+khoicrtp@users.noreply.github.com>
2021-10-12 04:41:31 +0700

    xong 1

--- LetterFreq/LetterFreq.py
+++ LetterFreq/LetterFreq.py
@@ -1,26 +1,37 @@
 import copy
-import pickle
-import gib_detect_train
+import time
+import os
 
-model_data = pickle.load(open('gib_model.pki', 'rb'))
+import numpy as np
+import matplotlib.pyplot as plt
+
+from threading import Thread
+import threading
+import time
+
 cipher = "NWVKIF UWVYC TWY AFYXAKIL CD QKIWCI UNVCCIKYEP HP EIK QEANLEDDL CDP IWKCE XDFP XDYIP. UNVCCIKYEP'Y AFACAWN LIYAMF, XVHNAYEIL DFNAFI HP UWVYC, KIUIKY CD EIK WY 'XDYIP' WY TINN. EIK QVCAI OWKG, CEKII XAFG WFL QPWF HVCCIKUNAIY, AY YAOANWK CD CEWC DU CEI XDFP YGP YGAOOIK. "
 expectedResult = "lauren faust was inspired to create fluttershy by her childhood toy earth pony posey. fluttershy's initial design, published online by faust, refers to her as 'posey' as well. her cutie mark, three pink and cyan butterflies, is similar to that of the pony sky skimmer."
 cipherTest = "abca"
 
-def checkValidWord(word):
-    model_mat = model_data['mat']
-    threshold = model_data['thresh']
-    return (gib_detect_train.avg_transition_prob(word, model_mat) > threshold)
+
+def defineLetterDict(cipher):
+    res = dict()
+    for i in range(0, len(cipher)):
+        if (cipher[i] not in res and cipher[i].isalpha() == True):
+            obj = {cipher[i]: countFreq(cipher, cipher[i])}
+            res.update(obj)
+    result = sorted(res.items(), key=lambda x: x[1], reverse=True)
+    return result
 
 def defineLetterList(cipher):
     res = []
-    for i in range(0, len(cipher)):
-        if (cipher[i] not in res):
-            res.append(cipher[i])
+    letterDict=defineLetterDict(cipher)
+    for i in range(0, len(letterDict)):
+        res.append(letterDict[i][0])
+    #print(res)
     return res
 
-
-def userManualReplace(cipher):
+def manualReplaceSolve(cipher):
     res = ""
     letterList = defineLetterList(cipher)
     expectedLetters = copy.deepcopy(letterList)
@@ -30,14 +41,22 @@ def userManualReplace(cipher):
             expectedLetters[i] = input(temp)
         else:
             expectedLetters[i] = letterList[i]
-    print(expectedLetters)
+    #print(expectedLetters)
     for i in range(0, len(cipher)):
         for j in range(0, len(letterList)):
             if(cipher[i] == letterList[j]):
                 res += expectedLetters[j]
                 break
-    print("expected output: ", res)
+    #print("expected output: ", res)
+    print("CIPHER TEXT: ", cipher, "\n")
+
+    print("DECODED OUTPUT: ", res, "\n")
+
+    print("EXPECTED OUTPUT:", expectedResult)
     return res
+
+# replace(cipher)
+
 
 standardLetterFreq = ['e', 't', 'a', 'o', 'i',
                       'n', 's', 'h', 'r', 'd',
@@ -45,23 +64,13 @@ standardLetterFreq = ['e', 't', 'a', 'o', 'i',
                       'f', 'g', 'y', 'p', 'b',
                       'v', 'k', 'j', 'x', 'q', 'z']
 
+
 def countFreq(cipher, letter):
     count = 0
     for i in range(0, len(cipher)):
         if letter == cipher[i]:
             count += 1
     return count
-
-
-def defineLetterDict(cipher):
-    res = dict()
-    for i in range(0, len(cipher)):
-        if (cipher[i] not in res):
-            obj = {cipher[i]: countFreq(cipher, cipher[i])}
-            res.update(obj)
-    result = sorted(res.items(), key=lambda x: x[1], reverse=True)
-    return result
-
 
 def toArray(cipher):
     a = []
@@ -85,29 +94,6 @@ def get_nth_key(dictionary, n=0):
             return key
     raise IndexError("dictionary index out of range")
 
-def createEmptyArray(n):
-    res=[]
-    for i in range(0,n):
-        res.append(0)
-    return res
-
-def collectWordList(a):
-    wordList=[]
-    temp=""
-    for i in range(0,len(a)):
-        if(a[i]!=" " and a[i]!="0"):
-            temp+=str(a[i])
-        else:
-            wordList.append(temp)
-            temp=""
-    return wordList
-
-def checkEachWordIsLegit(a):
-    wordList=collectWordList(a)
-    for i in range(len(a)):
-        if(checkValidWord(a[i])==False):
-            return False
-    return True
 
 def cipherLetterFreqSolve(cipher):
     letterDict = defineLetterDict(cipher)
@@ -120,104 +106,60 @@ def cipherLetterFreqSolve(cipher):
         else:
             expectedLetterDict[letterDict[i][0]] = letterDict[i][0]
 
-    res=createEmptyArray(len(cipher))
-    print(res)
-    
-    usedWord=[]
+    res = toArray(cipher)
 
     for i in range(0, len(res)):
-        for k in range(len(expectedLetterDict)):
-            j=k
+        for j in range(len(expectedLetterDict)):
             if(cipher[i] == letterDict[j][0]):
                 key = get_nth_key(expectedLetterDict, j)
                 res[i] = expectedLetterDict[key]
-                while(checkEachWordIsLegit(res)==False):
-                    if(j==len(expectedLetterDict)):
-                        print("CANT FIND SOLUTION")
-                        return
-                    j+=1
-                    key = get_nth_key(expectedLetterDict, j)
-                    res[i] = expectedLetterDict[key]
-                usedWord.append(expectedLetterDict[key])
                 break
     result = toString(res)
 
     return result
 
-standardLetterFreq = ['e', 't', 'a', 'o', 'i',
-                      'n', 's', 'h', 'r', 'd',
-                      'l', 'c', 'u', 'm', 'w',
-                      'f', 'g', 'y', 'p', 'b',
-                      'v', 'k', 'j', 'x', 'q', 'z']
-
-def replaceLetter(res,letter,toLetter):
-    for i in range(len(res)):
-        if res[i]==letter:
-            res[i]=toLetter
-    
-    if(checkEachWordIsLegit(res)==True):
-        return res
-    else: return 0
-
-def recurReplace(res, wordList, index):
-    tempWordList=wordList
-    if(len(wordList)==0):
-        return res
-    toLetter=tempWordList.pop()
-    newRes=replaceLetter(res, letter, toLetter)
-    if(newRes!=0):
-        return recurReplace(res, tempWordList, index)
-    
-def tryReplace(res,i,usedLetter):
-    flag=0
-    j=0
-    while(flag==0):
-        while(standardLetterFreq[j] in usedLetter):
-            j+=1
-        res[i]=standardLetterFreq[j]
-        #wordList=collectWordList(res)
-        if(checkEachWordIsLegit(res)):
-            flag=1
-            usedLetter.append()
-
-def createAbstractArray(cipher):
-    res=[]
-    for i in range(0,len(cipher)):
-        if(cipher[i].isalpha()==True):
-            res.append("*")
-        else:
-            res.append(cipher[i])
+##DRAW
+def getObjKeys(data):
+    res=list()
+    for i in range(len(data)):
+        res.append(data[i][0])
     return res
 
+def getObjValues(data):
+    res=list()
+    for i in range(len(data)):
+        res.append(data[i][1])
+    return res
+
+def drawGraph():
+    # creating the dataset
+    wordList = defineLetterDict(cipher)
+    courses = getObjKeys(wordList)
+    values = getObjValues(wordList)
+
+    fig = plt.figure(figsize=(10, 5))
+
+# creating the bar plot
+    plt.bar(courses, values, color='maroon',
+        width=0.4)
+
+    plt.xlabel("Letter")
+    plt.ylabel("Frequency")
+    plt.title("LETTER FREQUENCY")
+    plt.show()
+
+if __name__=='__main__':
+    t1=threading.Thread(target=drawGraph, args=())
+    t1.start()
+    time.sleep(3)
+    os.system('cls||clear')
+    wordList = defineLetterDict(cipher)
+    print("WORDLIST FREQ:", wordList)
+    print("STANDARD LETTER FREQ:", standardLetterFreq)
+    mode=int(input("CHOOSE MODE:\n1.Manual\n2.Automatic\n"))
+    if mode==1:
+        t2=threading.Thread(target=manualReplaceSolve, args=(cipher,))
+
+    #manualReplaceSolve(cipher)
+    t2.start()
     
-    
-    
-
-def cipherLetterFreqSolve2(cipher):
-    letterDict = defineLetterDict(cipher)
-    j = 0
-    res=toArray(cipher)
-    usedLetter=[]
-    for i in range(len(letterDict)):
-        if(letterDict[i][0].isalpha() == True):
-            #expectedLetterDict[letterDict[i][0]] = standardLetterFreq[j]
-            res[i]=standardLetterFreq[j]
-            if(checkEachWordIsLegit(res)==False):
-                k=j
-                while(checkEachWordIsLegit(res)==False):
-                    k+=1
-                    res[i]=standardLetterFreq[k]
-                usedLetter=standardLetterFreq[k]
-            j += 1
-        else:
-            expectedLetterDict[letterDict[i][0]] = letterDict[i][0]
-
-    return result
-
-print("CIPHER TEXT: ", cipher, "\n")
-
-#print("DECODED OUTPUT: ", cipherLetterFreqSolve(cipher), "\n")
-
-#print("EXPECTED OUTPUT:", expectedResult)
-
-print(createAbstractArray(cipher))
---
+++ LetterFreq/test.py
@@ -0,0 +1,189 @@
+import copy
+import pickle
+import gib_detect_train
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+model_data = pickle.load(open('gib_model.pki', 'rb'))
+cipher = "NWVKIF UWVYC TWY AFYXAKIL CD QKIWCI UNVCCIKYEP HP EIK QEANLEDDL CDP IWKCE XDFP XDYIP. UNVCCIKYEP'Y AFACAWN LIYAMF, XVHNAYEIL DFNAFI HP UWVYC, KIUIKY CD EIK WY 'XDYIP' WY TINN. EIK QVCAI OWKG, CEKII XAFG WFL QPWF HVCCIKUNAIY, AY YAOANWK CD CEWC DU CEI XDFP YGP YGAOOIK. "
+expectedResult = "lauren faust was inspired to create fluttershy by her childhood toy earth pony posey. fluttershy's initial design, published online by faust, refers to her as 'posey' as well. her cutie mark, three pink and cyan butterflies, is similar to that of the pony sky skimmer."
+cipherTest = "abca"
+
+
+def countFreq(cipher, letter):
+    count = 0
+    for i in range(0, len(cipher)):
+        if letter == cipher[i]:
+            count += 1
+    return count
+
+
+def defineLetterDict(cipher):
+    res = dict()
+    for i in range(0, len(cipher)):
+        if (cipher[i] not in res and cipher[i].isalpha() == True):
+            obj = {cipher[i]: countFreq(cipher, cipher[i])}
+            res.update(obj)
+    result = sorted(res.items(), key=lambda x: x[1], reverse=True)
+    return result
+
+
+def checkValidWord(word):
+    model_mat = model_data['mat']
+    threshold = model_data['thresh']
+    return (gib_detect_train.avg_transition_prob(word, model_mat) > threshold)
+
+
+def collectWordList(a):
+    wordList = []
+    temp = ""
+    for i in range(0, len(a)):
+        if(a[i] != " " and a[i] != "*" and a[i].isalpha() == True):
+            temp += str(a[i])
+        else:
+            if temp != "":
+                wordList.append(temp)
+            temp = ""
+    wordList.append(temp)
+
+    return wordList
+
+
+def checkEachWordIsLegit(a):
+    wordList = collectWordList(a)
+    print("WORDLIST CHECK:", wordList)
+    for i in range(0,len(wordList)):
+        print(wordList[i]," is:", checkValidWord(wordList[i]))
+        if(checkValidWord(wordList[i]) == False):
+            return False
+    return True
+
+
+def replaceLetter(cipherArray, dummyArray, letter, toLetter):
+    print("REPLACING ",letter, " to ", toLetter)
+    for i in range(len(dummyArray)):
+        if cipherArray[i] == letter[0]:
+            dummyArray[i] = toLetter
+
+    if(checkEachWordIsLegit(dummyArray) == True):
+        # print(dummyArray)
+        return dummyArray
+    else:
+        return 0
+
+
+def toArray(cipher):
+    a = []
+    for i in range(0, len(cipher)):
+        a.append(cipher[i])
+    return a
+
+
+def createDummyArray(cipher):
+    a = []
+    for i in range(0, len(cipher)):
+        if(cipher[i].isalpha() == True):
+            a.append('*')
+        else:
+            a.append(cipher[i])
+    return a
+
+
+standardLetterFreq = ['e', 't', 'a', 'o', 'i',
+                      'n', 's', 'h', 'r', 'd',
+                      'l', 'c', 'u', 'm', 'w',
+                      'f', 'g', 'y', 'p', 'b',
+                      'v', 'k', 'j', 'x', 'q', 'z']
+
+
+def recurReplace(cipherArray, dummyArray, wordList, toWordList, deny):
+    if('*' not in dummyArray):
+        return dummyArray
+
+    tempDeny=copy.deepcopy(deny)
+    tempWordList = copy.deepcopy(wordList)
+    tempToWordList = copy.deepcopy(toWordList)
+    
+    letter = tempWordList[0]
+    tempWordList.remove(letter)
+    
+    toLetter = tempToWordList[0]
+    i = 0
+    cantReplaceFlag=0
+    while(toLetter in tempDeny):
+        i += 1
+        if i==len(tempToWordList):
+            cantReplaceFlag=1
+            break
+        toLetter = tempToWordList[i]
+    if cantReplaceFlag==1:
+        temp=wordList[0]
+        wordList[0]=wordList[1]
+        wordList[1]=temp
+        print("SWAPPED: ",wordList)
+        cantReplaceFlag=0
+        tempDeny=copy.deepcopy(deny)
+        return recurReplace(cipherArray, dummyArray, wordList, toWordList, deny)
+    tempToWordList.remove(toLetter)
+
+    tempDummy = replaceLetter(cipherArray, dummyArray, letter, toLetter)
+
+    if(tempDummy != 0):
+        tempDeny.append(toLetter)
+        print("DUMMY:", collectWordList(dummyArray), '\n')
+        return recurReplace(cipherArray, tempDummy, tempWordList, tempToWordList, tempDeny)
+    else:
+        deny.append(toLetter)
+        print("DUMMY:", collectWordList(dummyArray), '\n')
+        return recurReplace(cipherArray, dummyArray, wordList, toWordList, deny)
+
+def printResult(dummyArray):
+    result=collectWordList(dummyArray)
+    stringRes=""
+    print("CRACKED OUTPUT:")
+    for i in range (len(result)):
+        stringRes+=result[i]+" "
+    print(stringRes)
+    return stringRes
+
+def getObjKeys(data):
+    res=list()
+    for i in range(len(data)):
+        res.append(data[i][0])
+    return res
+
+def getObjValues(data):
+    res=list()
+    for i in range(len(data)):
+        res.append(data[i][1])
+    return res
+
+def drawGraph(data):
+    # creating the dataset
+    courses = getObjKeys(data)
+    values = getObjValues(data)
+
+    fig = plt.figure(figsize=(10, 5))
+
+# creating the bar plot
+    plt.bar(courses, values, color='maroon',
+        width=0.4)
+
+    plt.xlabel("Letter")
+    plt.ylabel("Frequency")
+    plt.title("LETTER FREQUENCY")
+    plt.show()
+
+wordList = defineLetterDict(cipher)
+print("WORDLIST:", wordList)
+print(wordList[0][0])
+#drawGraph(wordList)
+cipherArray = toArray(cipher)
+dummyArray = createDummyArray(cipher)
+toWordList = standardLetterFreq
+deny = []
+Result=recurReplace(cipherArray, dummyArray, wordList, toWordList, deny)
+
+printResult(dummyArray)
+
